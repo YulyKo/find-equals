5d36b4cae4be550cfd49b71926365b70f52a0beb
YulyKo <yuliakotenko1.0@gmail.com>
2020-11-14 13:03:23 +0200

    added simple game panel

---
+++ src/views/GamePanel.vue
@@ -0,0 +1,20 @@
+<template>
+  <main>
+    <cards-vue></cards-vue>
+  </main>
+</template>
+
+<script>
+import CardVue from '@/components/Card.vue';
+
+export default {
+  name: 'GamePanel',
+  components: {
+    cardsVue: CardVue,
+  },
+};
+</script>
+
+<style>
+
+</style>
