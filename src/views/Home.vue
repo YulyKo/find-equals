9119328b544053cb535fe5212ed15140db7af61b
YulyKo <yuliakotenko1.0@gmail.com>
2020-11-14 13:04:01 +0200

    changed Home.vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,21 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <main>
+    <h1>Welcome to game!</h1>
+    <game-panel></game-panel>
+  </main>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue';
+import GamePanelVue from './GamePanel.vue';
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld,
+    gamePanel: GamePanelVue,
   },
 };
 </script>
+
+<style>
+
+</style>
