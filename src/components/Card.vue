96cdb541d389541b67315a14bc88d6ef9e09945a
YulyKo <yuliakotenko1.0@gmail.com>
2020-11-14 13:00:41 +0200

    added base of card

---
+++ src/components/Card.vue
@@ -0,0 +1,16 @@
+<template>
+  <section>
+    <img src="@/assets/preview-card.png" alt="card">
+    <h2>0</h2>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'Card',
+};
+</script>
+
+<style>
+
+</style>
