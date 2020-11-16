6bb1c19630437a9a51ccd5c9f3fa3f62af1669c2
YulyKo <yuliakotenko1.0@gmail.com>
2020-11-16 20:00:26 +0200

    added base logic for the timer

--- src/components/Timer.vue
+++ src/components/Timer.vue
@@ -1,10 +1,30 @@
 <template>
-  <time datetime="20:00">20:00</time>
+  <section class="flex timer">
+    <time class="text" :datetime="time">{{ time }}</time>
+  </section>
 </template>
 
 <script>
 export default {
   name: 'Timer',
+  data() {
+    return {
+      time: 60,
+    };
+  },
+  created() {
+    this.countTime();
+  },
+  methods: {
+    countTime() {
+      if (this.time > 0) {
+        setTimeout(() => {
+          this.time -= 1;
+          this.countTime();
+        }, 1000);
+      }
+    },
+  },
 };
 </script>
 
