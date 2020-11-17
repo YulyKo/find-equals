27ea6bc47291c3d024bce6272d69f9936dcde0fb
YulyKo <yuliakotenko1.0@gmail.com>
2020-11-17 16:55:05 +0200

    added a resetGame method

--- src/components/ResetButton.vue
+++ src/components/ResetButton.vue
@@ -1,9 +1,26 @@
 <template>
-  <button type="button">Reset</button>
+  <button type="button" @click="resetGame()"><slot></slot></button>
 </template>
 
 <script>
+import { mapMutations } from 'vuex';
+
 export default {
   name: 'ResetButton',
+  methods: {
+    ...mapMutations({
+      setStep: 'setStep',
+      setMessage: 'setMessage',
+      setResultTime: 'setResultTime',
+      setTime: 'setTime',
+    }),
+    resetGame() {
+      this.setStep(0);
+      this.setMessage('');
+      this.setResultTime(0);
+      this.setTime(0);
+      this.$router.push('/');
+    },
+  },
 };
 </script>
