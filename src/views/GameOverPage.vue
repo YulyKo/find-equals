1161fe213fd94d7faf2d49392026d3a1ab387487
YulyKo <yuliakotenko1.0@gmail.com>
2020-11-17 17:25:05 +0200

    inserted reserButton on GameOverPage

--- src/views/GameOverPage.vue
+++ src/views/GameOverPage.vue
@@ -1,14 +1,19 @@
 <template>
-  <game-over-panel></game-over-panel>
+  <main>
+    <game-over-panel></game-over-panel>
+    <reset-button>Try again</reset-button>
+  </main>
 </template>
 
 <script>
 import GameOverPanelVue from '../components/GameOverPanel.vue';
+import ResetButtonVue from '../components/ResetButton.vue';
 
 export default {
   name: 'GameOverPage',
   components: {
     gameOverPanel: GameOverPanelVue,
+    resetButton: ResetButtonVue,
   },
 };
 </script>
